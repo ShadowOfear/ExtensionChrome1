bc45141d705786b84b73b3214b1ee6d28b674102
BOISSON <florentinboisson@yahoo.fr>
2015-05-24 13:21:01 +0200

    Mise a jour et ajout

--- extensionGC_graph_TopicRank.js
+++ extensionGC_graph_TopicRank.js
@@ -8,13 +8,13 @@ function GraphTR(tableauFinal) {
 	var bool =0;
 	for(j in tableauFinal){
 
-
 		for (var parcours = 2 ; parcours <= tableauFinal[j].length ; parcours +=3) { 
 
-			if ((tableauFinal[j][parcours-1]== "NN") || (tableauFinal[j][parcours-1]== "JJ") || 
-					(tableauFinal[j][parcours-1] == "CD") || 
+			if ((tableauFinal[j][parcours-1] == "NN") ||
+					(tableauFinal[j][parcours-1] == "JJ") ||
 					(tableauFinal[j][parcours-1] == "RB")  || 
-					(tableauFinal[j][parcours-1] == "NNS") || (tableauFinal[j][parcours-1] == "NNP")) {
+					(tableauFinal[j][parcours-1] == "NNS") ||
+					(tableauFinal[j][parcours-1] == "NNP")) {
 				motTesteNoeud= tableauFinal[j][parcours];
 
 				bool = 0;
@@ -39,27 +39,35 @@ function GraphTR(tableauFinal) {
 
 	for(k in tableauFinal){	
 
-		/* DEBUT PREMIER MOT DE LA PHRASE */
-
-		if(tableauFinal[k][motDebutDePhrase] in noeud){
-			var motDebutDePhrase = tableauFinal[k][2];	
-			if ((tableauFinal[k][4] == "NN") || (tableauFinal[k][4] == "JJ")
-					|| (tableauFinal[k][4] == "CD") || (tableauFinal[k][4] == "RB")
-					|| (tableauFinal[k][4] == "NNS") || (tableauFinal[k][4] == "NNP")) {
-				bool = 0;	
-				for((tableauFinal[k][5]) in noeud[motDebutDePhrase]){
-					//for (var goFast = 0; goFast<noeud[motDebutDePhrase].length; goFast += 1){
-					//if ((tableauFinal[k][5]) == noeud[motDebutDePhrase][goFast]) {   
-					bool = 1;
-					//}
-				}
-
-				var leMot = tableauFinal[k][5];
-				if(bool == 0){
-					noeud[motDebutDePhrase].push(leMot);
-					noeud[motDebutDePhrase][leMot] = 1;  
-				} else {
-					noeud[motDebutDePhrase][leMot] += 1;
+		/* DEBUT PREMIERS MOT DE LA PHRASE */
+		var longueurMin = 17;
+		var longueurMax = tableauFinal[k].length -17;
+		if(tableauFinal[k].length <17){
+		longueurMin = tableauFinal[k].length;
+			if(tableauFinal[k].length<17){
+				longueurMax =tableauFinal[k].length;
+			}
+		}
+/* jusqu'ici, c'est ok :) */
+		for(var i = 2; i<=longueurMin; i +=3){
+			if(tableauFinal[k][i] in noeud){
+				var motDebutDePhrase = tableauFinal[k][i];
+				for(j=i; j>2; j = j-3){
+					if ((tableauFinal[k][j-4] == "NN") || (tableauFinal[k][j-4] == "JJ")
+						|| (tableauFinal[k][j-4] == "RB")
+						|| (tableauFinal[k][j-4] == "NNS") || (tableauFinal[k][j-4] == "NNP")) {
+						bool = 0;	
+						for((tableauFinal[k][j]) in noeud[motDebutDePhrase]){
+							bool = 1;
+						}
+						var leMot = tableauFinal[k][j];
+						if(bool == 0){
+							noeud[motDebutDePhrase].push(leMot);
+							noeud[motDebutDePhrase][leMot] = 1;  
+						} else {
+							noeud[motDebutDePhrase][leMot] += 1;
+						}
+					}
 				}
 			}
 		}
@@ -77,9 +85,11 @@ function GraphTR(tableauFinal) {
 			if(tableauFinal[k][indiceActuel] in noeud){
 
 				if ((tableauFinal[k][indiceActuel-4] == "NN") || (tableauFinal[k][indiceActuel-4] == "JJ") 
-						|| (tableauFinal[k][indiceActuel-4] == "CD")
 						||  (tableauFinal[k][indiceActuel-4] == "RB")
 						|| (tableauFinal[k][indiceActuel-4] == "NNS") || (tableauFinal[k][indiceActuel-4] == "NNP")) {
+					if(tableauFinal[k][indiceActuel] == "helicopt"){
+
+					}
 					bool = 0;	
 					//for((tableauFinal[k][indiceActuel-3]) in noeud[motActuel]){
 					for (var goFast = 0; goFast<noeud[motActuel].length; goFast += 1){
@@ -100,7 +110,6 @@ function GraphTR(tableauFinal) {
 				}
 
 				if ((tableauFinal[k][indiceActuel+2] == "NN") || (tableauFinal[k][indiceActuel+2]  == "JJ") 
-						|| (tableauFinal[k][indiceActuel+2]  == "CD") 
 						||  (tableauFinal[k][indiceActuel+2]  == "RB")
 						|| (tableauFinal[k][indiceActuel+2]  == "NNS") || (tableauFinal[k][indiceActuel+2]  == "NNP")) {
 
@@ -139,7 +148,6 @@ function GraphTR(tableauFinal) {
 
 
 			if ((tableauFinal[k][indiceDernierMot-4]  == "NN") || (tableauFinal[k][indiceDernierMot-4]  == "JJ")
-					|| (tableauFinal[k][indiceDernierMot-4]  == "CD") 
 					|| (tableauFinal[k][indiceDernierMot-4]  == "RB")  
 					|| (tableauFinal[k][indiceDernierMot-4]  == "NNS") || (tableauFinal[k][indiceDernierMot-4]  == "NNP")) {
 
