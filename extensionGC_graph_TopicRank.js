846f30960d2727974ddbc75dc32cd10512a60bdb
BOISSON <florentinboisson@yahoo.fr>
2015-05-25 14:46:02 +0200

    MaJ
    Version 1.0 du graphe TopicRank;
    Ajout/comparaison pour le html de test

--- extensionGC_graph_TopicRank.js
+++ extensionGC_graph_TopicRank.js
@@ -1,4 +1,4 @@
-function GraphTR(tableauFinal) {
+function GraphTopicR(tableauFinal) {
 
 	var noeud = new Array;
 
@@ -38,24 +38,21 @@ function GraphTR(tableauFinal) {
 
 
 	for(k in tableauFinal){	
-
-		/* DEBUT PREMIERS MOT DE LA PHRASE */
 		var longueurMin = 17;
-		var longueurMax = tableauFinal[k].length -17;
+		var longueurMax = tableauFinal[k].length -18;
 		if(tableauFinal[k].length <17){
-		longueurMin = tableauFinal[k].length;
-			if(tableauFinal[k].length<17){
-				longueurMax =tableauFinal[k].length;
-			}
+			longueurMin = tableauFinal[k].length;
+			longueurMax = tableauFinal[k].length;
 		}
-/* jusqu'ici, c'est ok :) */
-		for(var i = 2; i<=longueurMin; i +=3){
+		
+		/* DEBUT PREMIERS MOT DE LA PHRASE */
+		for(var i = 2; i<=longueurMin -3; i +=3){
 			if(tableauFinal[k][i] in noeud){
 				var motDebutDePhrase = tableauFinal[k][i];
-				for(j=i; j>2; j = j-3){
-					if ((tableauFinal[k][j-4] == "NN") || (tableauFinal[k][j-4] == "JJ")
-						|| (tableauFinal[k][j-4] == "RB")
-						|| (tableauFinal[k][j-4] == "NNS") || (tableauFinal[k][j-4] == "NNP")) {
+				for(j=i -3; j>2; j = j-3){
+					if ((tableauFinal[k][j-1] == "NN") || (tableauFinal[k][j-1] == "JJ")
+						|| (tableauFinal[k][j-1] == "RB")
+						|| (tableauFinal[k][j-1] == "NNS") || (tableauFinal[k][j-1] == "NNP")) {
 						bool = 0;	
 						for((tableauFinal[k][j]) in noeud[motDebutDePhrase]){
 							bool = 1;
@@ -71,57 +68,65 @@ function GraphTR(tableauFinal) {
 				}
 			}
 		}
-
+		
+		for(var i = 2; i<=longueurMin -3; i +=3){
+			if(tableauFinal[k][i] in noeud){
+				var motDebutDePhrase = tableauFinal[k][i];
+				for(j=i +3; j<=17 || j<=longueurMax; j = j+=3){
+					if ((tableauFinal[k][j-1] == "NN") || (tableauFinal[k][j-1] == "JJ")
+						|| (tableauFinal[k][j-1] == "RB")
+						|| (tableauFinal[k][j-1] == "NNS") || (tableauFinal[k][j-1] == "NNP")) {
+						bool = 0;	
+						for((tableauFinal[k][j]) in noeud[motDebutDePhrase]){
+							bool = 1;
+						}
+						var leMot = tableauFinal[k][j];
+						if(bool == 0){
+							noeud[motDebutDePhrase].push(leMot);
+							noeud[motDebutDePhrase][leMot] = 1;  
+						} else {
+							noeud[motDebutDePhrase][leMot] += 1;
+						}
+					}
+				}
+			}
+		}
 		/* FIN PREMIER MOT DE LA PHRASE */
+
 
 		/* DEBUT MOT MILIEU PHRASE */
 		var indiceDernierMot = (tableauFinal[k].length)-1;
 		var indiceAvantDernierMot = indiceDernierMot - 3;
 
-
-		for (var indiceActuel = 5 ; indiceActuel <= indiceAvantDernierMot ; indiceActuel +=3 ) {
-
-			var motActuel = tableauFinal[k][indiceActuel];
-			if(tableauFinal[k][indiceActuel] in noeud){
-
-				if ((tableauFinal[k][indiceActuel-4] == "NN") || (tableauFinal[k][indiceActuel-4] == "JJ") 
-						||  (tableauFinal[k][indiceActuel-4] == "RB")
-						|| (tableauFinal[k][indiceActuel-4] == "NNS") || (tableauFinal[k][indiceActuel-4] == "NNP")) {
-					if(tableauFinal[k][indiceActuel] == "helicopt"){
-
-					}
+		for(var i = longueurMin; i<=longueurMax; i +=3){
+			if(tableauFinal[k][i] in noeud){
+				var motActuel = tableauFinal[k][i];
+				for(j=i -3; j>=17 || j>=(i-17); j = j-3){
+					if ((tableauFinal[k][j-1] == "NN") || (tableauFinal[k][j-1] == "JJ") 
+						||  (tableauFinal[k][j-1] == "RB")
+						|| (tableauFinal[k][j-1] == "NNS") || (tableauFinal[k][j-1] == "NNP")) {
 					bool = 0;	
-					//for((tableauFinal[k][indiceActuel-3]) in noeud[motActuel]){
-					for (var goFast = 0; goFast<noeud[motActuel].length; goFast += 1){
-
-						if ((tableauFinal[k][indiceActuel-3]) == noeud[motActuel][goFast]) { 
-							bool = 1;	
+						for((tableauFinal[k][j]) in noeud[motActuel]){
+								bool = 1;
 						}
-					}
-					var leMot = tableauFinal[k][indiceActuel-3];
-					if(bool == 0){
-						noeud[motActuel].push(leMot);
-						noeud[motActuel][leMot] = 1;
-
-					}
-					else {
-						noeud[motActuel][leMot] += 1;
+						var leMot = tableauFinal[k][j];
+						if(bool == 0){
+							noeud[motActuel].push(leMot);
+							noeud[motActuel][leMot] = 1;
+						}else {
+							noeud[motActuel][leMot] += 1;
+						}
 					}
 				}
-
-				if ((tableauFinal[k][indiceActuel+2] == "NN") || (tableauFinal[k][indiceActuel+2]  == "JJ") 
-						||  (tableauFinal[k][indiceActuel+2]  == "RB")
-						|| (tableauFinal[k][indiceActuel+2]  == "NNS") || (tableauFinal[k][indiceActuel+2]  == "NNP")) {
-
-
-					bool = 0;	
-
-					for (var goFast = 0; goFast<noeud[motActuel].length; goFast += 1){
-						if ((tableauFinal[k][indiceActuel+3]) == noeud[motActuel][goFast]) { 
-							bool = 1;	
-						}
+			for(l=i +3; l<longueurMax || l<(i+17); l = l+3){
+				if ((tableauFinal[k][l-1] == "NN") || (tableauFinal[k][l-1]  == "JJ") 
+						||  (tableauFinal[k][l-1]  == "RB")
+						|| (tableauFinal[k][l-1]  == "NNS") || (tableauFinal[k][l-1]  == "NNP")) {
+					bool = 0;
+					for((tableauFinal[k][l]) in noeud[motActuel]){ 
+							bool = 1;
 					}
-					var leMot = tableauFinal[k][indiceActuel+3];
+					var leMot = tableauFinal[k][l];
 					if(bool == 0){
 						noeud[motActuel].push(leMot);
 						noeud[motActuel][leMot] = 1;
@@ -134,42 +139,60 @@ function GraphTR(tableauFinal) {
 				}
 			}  
 		}
-
+	}
 		/* FIN MOT MILIEU PHRASE */
 
 
 		/* DEBUT DERNIER MOT */
-
-		var motFinDePhrase = tableauFinal[k][indiceDernierMot];
-
-
-
-		if(motFinDePhrase in noeud){
-
-
-			if ((tableauFinal[k][indiceDernierMot-4]  == "NN") || (tableauFinal[k][indiceDernierMot-4]  == "JJ")
-					|| (tableauFinal[k][indiceDernierMot-4]  == "RB")  
-					|| (tableauFinal[k][indiceDernierMot-4]  == "NNS") || (tableauFinal[k][indiceDernierMot-4]  == "NNP")) {
-
-				bool = 0;	
-
-				for (var goFast = 0; goFast<noeud[motFinDePhrase].length; goFast += 1){
-					if ((tableauFinal[k][indiceDernierMot-3]) == noeud[motFinDePhrase][goFast]) { 
-						bool = 1;	
+	for(var i = longueurMax; i<=(tableauFinal[k].length -1); i +=3){
+			if(tableauFinal[k][i] in noeud){
+				var motFinDePhrase = tableauFinal[k][i];
+				for(j=i -3; j>=(i-17) || j>= longueurMin; j = j-3){	
+					var motFinDePhrase = tableauFinal[k][i];
+					if(motFinDePhrase in noeud){
+						if ((tableauFinal[k][j-1]  == "NN") || (tableauFinal[k][j-1]  == "JJ")
+							|| (tableauFinal[k][j-1]  == "RB")  
+							|| (tableauFinal[k][j-1]  == "NNS") || (tableauFinal[k][j-1]  == "NNP")) {
+							bool = 0;	
+							for((tableauFinal[k][j]) in noeud[motFinDePhrase]){  
+								bool = 1;	
+							}
+							var leMot = tableauFinal[k][j];
+							if(bool == 0){
+								noeud[motFinDePhrase].push(leMot);
+								noeud[motFinDePhrase][leMot] = 1;
+							} else {
+								noeud[motFinDePhrase][leMot] += 1;
+							}
+						}
 					}
 				}
-				var leMot = tableauFinal[k][indiceDernierMot-3];
-				if(bool == 0){
-					noeud[motFinDePhrase].push(leMot);
-					noeud[motFinDePhrase][leMot] = 1;
-				} else {
-					noeud[motFinDePhrase][leMot] += 1;
-				}
 
+				for(j=i +3; j<=(tableauFinal[k].length -1); j += 3){	
+					var motFinDePhrase = tableauFinal[k][i];
+					if(motFinDePhrase in noeud){
+						if ((tableauFinal[k][j-1]  == "NN") || (tableauFinal[k][j-1]  == "JJ")
+							|| (tableauFinal[k][j-1]  == "RB")  
+							|| (tableauFinal[k][j-1]  == "NNS") || (tableauFinal[k][j-1]  == "NNP")) {
+							bool = 0;	
+							for((tableauFinal[k][j]) in noeud[motFinDePhrase]){  
+								bool = 1;	
+							}
+							var leMot = tableauFinal[k][j];
+							if(bool == 0){
+								noeud[motFinDePhrase].push(leMot);
+								noeud[motFinDePhrase][leMot] = 1;
+							} else {
+								noeud[motFinDePhrase][leMot] += 1;
+							}
+						}
+					}
+				} 
 			}
 		}
-
-	}				
+	}
 	/* FIN DERNIER MOT */
+	
+	
 	return noeud;
 }
